ab4c91249f79f87b9babdebdd373878d6387fe2b
Divya Kulkarni <44868354+divya-kulkarni@users.noreply.github.com>
2021-04-07 18:33:10 +0530

    changed Geosuggest

--- source_code/src/components/Homepage/Navbar/searchBar.js
+++ source_code/src/components/Homepage/Navbar/searchBar.js
@@ -7,6 +7,36 @@ import Geosuggest from "react-geosuggest";
 import "./navbar.css";
 import { Search } from "react-bootstrap-icons";
 
+const onFocus = () => console.log("onFocus");
+
+/**
+ * When the input loses focus
+ */
+const onBlur = (value) => console.log("onBlur", value);
+
+/**
+ * When the input got changed
+ */
+const onChange = (value) => console.log(`input changes to : ${value}`);
+
+/**
+ * When a suggest got selected
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const onSuggestSelect = (suggest) => console.log(suggest);
+
+/**
+ * When there are no suggest results
+ */
+const onSuggestNoResults = (userInput) =>
+	console.log(`onSuggestNoResults for : ${userInput}`);
+
+const fixtures = [
+	{ label: "New York", location: { lat: 40.7033127, lng: -73.979681 } },
+	{ label: "Rio", location: { lat: -22.066452, lng: -42.9232368 } },
+	{ label: "Tokyo", location: { lat: 35.673343, lng: 139.710388 } },
+];
+
 function SearchBar() {
 	return (
 		<div>
@@ -43,7 +73,14 @@ function SearchBar() {
 							placeholder="Enter keywords.."
 							required
 						/>
-						<Geosuggest />
+						<Geosuggest
+							onFocus={onFocus}
+							onBlur={onBlur}
+							onChange={onChange}
+							onSuggestSelect={onSuggestSelect}
+							onSuggestNoResults={onSuggestNoResults}
+							radius="20"
+						/>
 						<InputGroup.Text className="search-button">
 							<Search />
 						</InputGroup.Text>
