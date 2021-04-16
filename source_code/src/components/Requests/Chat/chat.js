d286b4e71ff70a130ac41d357a423ad1bf4270a7
Akhil Suresh <akhilf4@gmail.com>
2021-04-16 19:09:45 +0530

    fixed google signin profile pic issue

--- source_code/src/components/Requests/Chat/chat.js
+++ source_code/src/components/Requests/Chat/chat.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./chat.css";
 
 function Chat() {
-	return <div className="chat-container"></div>;
+  return (
+    <div className="chat-container">
+      <center>
+        <h4 className="chat-heading">Recent Messages</h4>
+      </center>
+    </div>
+  );
 }
 
 export default Chat;
