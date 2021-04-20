125637276f9421ae7c4b05c78feba45b3d97b6c4
Akhil Suresh <akhilf4@gmail.com>
2021-04-20 10:21:50 +0530

    removed chat

--- source_code/src/components/Requests/Chat/chat.js
+++ source_code/src/components/Requests/Chat/chat.js
@@ -6,41 +6,14 @@ import { compose } from "redux";
 import { motion } from "framer-motion";
 import "./chat.css";
 
-const Chat = ({ chatList, user, userList }) => {
-  if (chatList && userList) {
-    const userDetails = new Map(
-      userList.map((obj) => [
-        obj.id,
-        { dp: obj.profile_picture, name: obj.name },
-      ])
-    );
-    return (
-      <div className="chat-container">
-        <center>
-          <h4 className="chat-heading">Recent Messages</h4>
-        </center>
-        {chatList.map((item, index) => {
-          if (item.from == user || item.to == user) {
-            var get_id;
-            item.from == user ? (get_id = item.to) : (get_id = item.from);
-            var name = userDetails.get(get_id);
-            name = name.name;
-            return (
-              <motion.div
-                className="activity-notif"
-                whileHover={{ scale: 1.02 }}
-                key={index}
-              >
-                <p className="notif-message">{name}</p>
-              </motion.div>
-            );
-          }
-        })}
-      </div>
-    );
-  } else {
-    return <div>Loading</div>;
-  }
+const Chat = () => {
+  return (
+    <div className="chat-container">
+      <center>
+        <h4 className="chat-heading">Recent Messages</h4>
+      </center>
+    </div>
+  );
 };
 
 const mapStatetoProps = (state) => {
