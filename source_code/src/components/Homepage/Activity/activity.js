ca5c53e1701c675f035ae5a56d33d2778882362e
Akhil Suresh <akhilf4@gmail.com>
2021-05-10 17:05:40 +0530

    fixed notifications

--- source_code/src/components/Homepage/Activity/activity.js
+++ source_code/src/components/Homepage/Activity/activity.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { connect } from "react-redux";
 import { compose } from "redux";
@@ -22,7 +22,6 @@ import { slide as Menu } from "react-burger-menu";
 
 const Activity = ({
 	notifications,
-	user,
 	clickNotif,
 	menuOpen,
 	setMenuOpen,
@@ -33,27 +32,22 @@ const Activity = ({
 	const closeMenu = () => {
 		setMenuOpen(false);
 	};
-	var notifCount = 0;
 
-	const handleDismiss = (e) => {
+	const handleDismiss = () => {
 		notifications.forEach((notif) => {
-			if (notif.to_user_id === user) {
-				deleteNotif(notif);
-				console.log("Deleted: ", notif);
-			}
+			deleteNotif(notif);
 		});
-		notifCount = 0;
-		console.log("Notif count->", notifCount);
-		setEmptyNotif(true);
 	};
 
 	const handleDelete = (notif) => {
 		console.log(notif);
 		deleteNotif(notif);
-		--notifCount;
-		// console.log("Notif count->", notifCount);
-		if (notifCount == 0) setEmptyNotif(true);
 	};
+
+	useEffect(() => {
+		if (notifications && notifications.length === 0) setEmptyNotif(true);
+		else setEmptyNotif(false);
+	}, [notifications]);
 
 	return (
 		<Menu right isOpen={menuOpen} width={"25vw"}>
@@ -63,73 +57,83 @@ const Activity = ({
 				<Button
 					className="dismiss-button"
 					onClick={handleDismiss}
-					style={{ display: isEmptyNotif ? "hidden" : "block" }}
+					style={{ display: isEmptyNotif ? "none" : "block" }}
 				>
 					Dismiss All
 				</Button>
 
 				<AnimatePresence>
 					{notifications &&
-						notifications.map((item, index) => {
-							if (item.to_user_id === user && notifCount < 5) {
-								++notifCount;
-								// console.log("Notif count->", notifCount);
-								return (
-									<motion.div
-										className="activity-notif"
-										whileHover={{ scale: 1.05 }}
-										key={index}
-										initial={{ opacity: 0 }}
-										animate={{ opacity: 1 }}
-										exit={{
-											x: -500,
+						notifications.map((item, i) => {
+							return (
+								<motion.li
+									className="activity-notif"
+									whileHover={{ scale: 1.05 }}
+									key={item.id}
+									style={{ listStyle: "none" }}
+									variants={{
+										hidden: (i) => ({
 											opacity: 0,
+											y: -50 * i,
+										}),
+										visible: (i) => ({
+											opacity: 1,
+											y: 0,
 											transition: {
-												//duration: 0.5,
-												duration: 0.5 + index * 0.2,
+												delay: i * 0.025,
 											},
-										}}
+										}),
+										removed: (i) => ({
+											opacity: 0,
+											x: -220 * i,
+											transition: {
+												type: "tween",
+											},
+										}),
+									}}
+									initial="hidden"
+									animate="visible"
+									exit="removed"
+									custom={i}
+								>
+									<div
+										className="delete-notif-div"
+										onClick={() => handleDelete(item)}
 									>
-										<div
-											className="delete-notif-div"
-											onClick={(e) => handleDelete(item)}
-										>
-											<X
-												style={{
-													float: "right",
-													height: "8%",
-													width: "8%",
-													color: "black",
-													cursor: "pointer",
-												}}
-											/>
-										</div>
-										<p
-											className="notif-message"
-											onClick={() => {
-												clickNotif(item);
-												closeMenu();
-												history.push("/requests");
+										<X
+											style={{
+												float: "right",
+												height: "8%",
+												width: "8%",
+												color: "black",
+												cursor: "pointer",
 											}}
-											style={{ cursor: "pointer" }}
-										>
-											{item.message}
-										</p>
-										<p className="notif-timestamp">
-											{item.timestamp
-												.toDate()
-												.toLocaleTimeString("en-US", {
-													weekday: "short",
-													day: "numeric",
-													month: "numeric",
-													hour: "2-digit",
-													minute: "2-digit",
-												})}
-										</p>
-									</motion.div>
-								);
-							}
-							// if (notifCount > 0) setEmptyNotif(false);
+										/>
+									</div>
+									<p
+										className="notif-message"
+										onClick={() => {
+											clickNotif(item);
+											closeMenu();
+											history.push("/requests");
+										}}
+										style={{ cursor: "pointer" }}
+									>
+										{item.message}
+									</p>
+									<p className="notif-timestamp">
+										{item.timestamp
+											.toDate()
+											.toLocaleTimeString("en-US", {
+												weekday: "short",
+												day: "numeric",
+												month: "numeric",
+												hour: "2-digit",
+												minute: "2-digit",
+											})}
+									</p>
+								</motion.li>
+							);
 						})}
 				</AnimatePresence>
 			</div>
@@ -153,10 +157,12 @@ const mapStatetoProps = (state) => {
 
 export default compose(
 	connect(mapStatetoProps, mapDispatchToProps),
-	firestoreConnect([
+	firestoreConnect((props) => [
 		{
 			collection: "Notification",
 			orderBy: ["timestamp", "desc"],
+			where: [["to_user_id", "==", props.user]],
+			limit: 5,
 		},
 	])
 )(Activity);
